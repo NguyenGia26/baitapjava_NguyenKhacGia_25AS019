2eb5b84477f0be7604d30966878105a4d0306b9b
NguyenGia26 <emlairocket@gmail.com>
2025-10-17 08:37:42 +0700

    Commit ex 1.1

---
+++ src/ex11/Circle.java
@@ -0,0 +1,26 @@
+package ex11;
+
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+
+    public Circle() {
+
+    }
+
+    public Circle(double r) {
+        this.radius = r;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
