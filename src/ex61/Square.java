220541b7e21285d74d89b7f65ca9d99e8cc027ef
NguyenGia26 <emlairocket@gmail.com>
2025-11-23 20:38:58 +0700

    hoan thanh bai

---
+++ src/ex61/Square.java
@@ -0,0 +1,17 @@
+package ex61;
+
+public class Square extends Rectangle {
+    public Square( double side){};
+    public Square(double side , String color , boolean filled){};
+    public double getSide(){
+        return 0;
+    };
+    public void setSide(double side){};
+    public void setWidth(double side){};
+    public void setLength(double side){};
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
